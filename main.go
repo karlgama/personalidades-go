997bd7985a5473506e456a04853f53ed7151a216
kaique grangeiro <kaique.grangeiro@sp.senai.br>
2023-10-10 17:33:50 -0300

    add database

--- main.go
+++ main.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
+	"personalidades/database"
 	"personalidades/models"
 	"personalidades/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Algum nome", Historia: "historia"},
-		{Id: 2, Nome: "Algum nome 1", Historia: "historia 1"},
-	}
-
+	models.Personalidades = []models.Personalidade{}
+	database.ConectaComBancoDeDados()
 	fmt.Println("Iniciando servidor")
 	routes.HandleRequest()
 }
