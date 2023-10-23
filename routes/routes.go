3a7e867f87f075a79237b6a488b7c946af02cf63
kaique grangeiro <kaique.grangeiro@sp.senai.br>
2023-10-23 14:30:30 -0300

    add middleware

--- routes/routes.go
+++ routes/routes.go
@@ -4,16 +4,19 @@ import (
 	"log"
 	"net/http"
 	"personalidades/controllers"
+	"personalidades/middleware"
 
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasAsPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.Delete).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.Edit).Methods("Put")
 	r.HandleFunc("/api/personalidades", controllers.Create).Methods("Post")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
